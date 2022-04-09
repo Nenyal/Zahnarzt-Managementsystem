8feafe0ad40a309f7afc296440d51ad061a9c188
Selim <selimcankurtaran1406@gmail.com>
2022-04-09 16:15:09 +0300

    Ergaenzungen zur Patient- und Termin-Klasse, Ueberschreiben der equals-Methode in der Termin-Klasse.

--- Patient.java
+++ Patient.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Patient extends Person {
     ArrayList<Termin> termine = new ArrayList<>();
@@ -10,19 +12,42 @@ public class Patient extends Person {
         super(id, name, nachname, tel);
     }
 
-    public void addTermin(Termin t){
+    public void addTermin(Termin t) {
         termine.add(t);
+    }
+
+    public void deleteTermin(Termin t) {
+        termine.remove(searchTermin(t.getDatum(),t.getZeit()));
+    }
+
+    public Termin searchTermin(Date datum, LocalTime zeit) {
+        Termin ter = new Termin(datum,zeit);
+        for (Termin t : termine) {
+            if (t.equals(ter)){
+                return t;
+            }
+        }
+        return null;
     }
 
     public ArrayList<Termin> getTermine() {
         return termine;
     }
 
-    public void editPatient(String name,String nachname){
+    public void addVergangeneBehandlung(Behandlung b) {
+        vergangeneBehandlungen.add(b);
+    }
+
+    public ArrayList<Behandlung> getVergangeneBehandlungen() {
+        return vergangeneBehandlungen;
+    }
+
+    public void editPatient(String name, String nachname) {
         this.setName(name);
         this.setNachname(nachname);
     }
-    public void editPatient(int tel){
+
+    public void editPatient(int tel) {
         this.setTelefonNummer(tel);
     }
 }
